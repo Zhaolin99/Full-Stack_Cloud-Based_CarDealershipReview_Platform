cb27c7526f627a34bb29cb7df59c2f611dca3803
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-04-06 20:51:25 -0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,9 +1,5 @@
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
-
-
-# Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 # - Name
@@ -15,35 +11,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    founded_year = models.PositiveIntegerField(null=True, blank=True)
+    #founded_year = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.name} - Founded: {self.founded_year or 'N/A'}"
+        return str(self.id)
 
 # Car Model model
-class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One
-    dealer_id = models.IntegerField()
-    name = models.CharField(max_length=100)
-
-    CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('COUPE', 'Coupe'),
-        ('TRUCK', 'Truck'),
-    ]
-    type = models.CharField(max_length=100)
-    year = models.IntegerField(default=2023,
-                               validators=[
-                                MaxValueValidator(2023),
-                                MinValueValidator(2015)])
-
-    #color = models.CharField(max_length=30, default='Black')
-
-    def __str__(self):
-        return f"{self.car_make.name} {self.name} ({self.year})"
-
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -54,3 +27,22 @@ class CarModel(models.Model):
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('COUPE', 'Coupe'),
+        ('TRUCK', 'Truck'),
+    ]
+    types = models.CharField(max_length=100)
+    year = models.IntegerField(default=2023, validators=[
+                                                MaxValueValidator(2023),
+                                                MinValueValidator(2015)])
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name} ({self.year})"
