97d495ae03e8bc82efb252b2639b4ea91bcce603
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-04-14 16:09:09 -0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -8,7 +8,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    #founded_year = models.PositiveIntegerField(null=True, blank=True)
+    # founded_year = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return str(self.id)
