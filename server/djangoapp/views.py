540fa4877241a321754f6c012ec087c3bf224083
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-04-14 17:12:05 -0700

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -131,8 +131,13 @@ def get_dealer_reviews(request, dealer_id):
             if reviews is None:
                 return JsonResponse({"status": 200, "reviews": []})
             for review_detail in reviews:
-                response = analyze_review_sentiments(review_detail.get('review', ''))
-                review_detail['sentiment'] = response.get('sentiment', 'unknown')
+                review_text = review_detail.get('review', '')
+                response = analyze_review_sentiments(review_text)
+                
+                review_detail['sentiment'] = response.get(
+                    'sentiment',
+                    'unknown'
+                )
             return JsonResponse({
                 "status": 200,
                 "reviews": reviews
@@ -160,7 +165,7 @@ def add_review(request):
         except Exception:
             return JsonResponse({
                 "status": 401,
-                "message":" Error in posting review"
+                "message": "Error in posting review"
             })
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
