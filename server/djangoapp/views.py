84aa48b8bce4dc6a88a2d38ddba33d0ae9846f81
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-04-22 20:43:22 -0700

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -171,16 +171,16 @@ def get_dealer_reviews(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if not request.user.is_anonymous:
+    if (request.user.is_anonymous is False):
         data = json.loads(request.body)
         try:
-            # response = post_review(data)
-            post_review(data)
+            response = post_review(data)
             return JsonResponse({"status": 200})
-        except Exception:
-            return JsonResponse({
-                "status": 401,
-                "message": "Error in posting review"
-            })
+        except:
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
+        finally:
+            print("add_review request successful!")
     else:
-        return JsonResponse({"status": 403, "message": "Unauthorized"})
+        return JsonResponse({"status": 403,
+                             "message": "Unauthorized"})
